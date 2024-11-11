abf6abe7a84c67f7e11801ac92b0d95394064d89
Aloksingh3810 <135043980+Aloksingh3810@users.noreply.github.com>
2024-11-11 20:57:39 +0530

    Dynamic Pages for each page

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+from tempfile import template
+
 from flask import Flask, render_template,jsonify
 
 from database import load_jobs_from_db, load_job_from_db
@@ -15,7 +17,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_rishav():
     jobs = load_jobs_from_db()
-    return render_template('home.html', jobs=jobs, company_name="Career's")
+    return render_template('home.html', jobs=jobs)
 
 
 @ app.route("/api/jobs")
@@ -25,8 +27,12 @@ def list_jobs():
 
 @app.route("/job/<id>")
 def show_job(id):
-    job=load_job_from_db(id)
-    return jsonify(job)
+    jobs=load_job_from_db(id)
+
+    if not jobs:
+        return "Page Not Found", 404
+
+    return render_template('jobpage.html', job=jobs)
 
 
 if __name__ == "__main__":
